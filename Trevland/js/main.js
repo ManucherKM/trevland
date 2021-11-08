7e329450327dd3fb423c16b0a4e1cd203201b984
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2021-11-08 11:29:04 +0500

    Add files via upload

--- Trevland/js/main.js
+++ Trevland/js/main.js
@@ -3,27 +3,14 @@ $(document).ready(function(){
 		arrows:true,
 		dots:true,
 		slidesToShow:3,
-        slidesToScroll: 2,
+        slidesToScroll: 3,
 		autoplay: false,
 		speed:800,
         infinite: true,
         initialSlide: 2,
         waitforAnimate: false,
         centerMode: true,
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:2
-				}
-			},
-			{
-				breakpoint: 550,
-				settings: {
-					slidesToShow:1
-				}
-			}
-		]
+		
 	});
 });
 
